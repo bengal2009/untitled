abb6cb52d345c7adab729863504416255c96fb84
blin <bennylin@outlook.com>
2014-12-30 17:53:16 +0800

    測試

--- src/com/company/SimpleTest.java
+++ src/com/company/SimpleTest.java
@@ -1,10 +1,22 @@
 package com.company;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 /**
  * Created by blin on 2014/12/30.
  */
 public class SimpleTest {
     public static void main(String[] args){
+        Integer i;
+        ArrayList a1=new ArrayList();
+        a1.add(new Integer(1));
+        a1.add(new Integer(2));
+        a1.add(new Integer(3));
+
+
+
+
             System.out.println("1122233111");
 
 
