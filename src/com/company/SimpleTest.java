f3114eb5a444a74a0f2c936b94501f65bc45179f
blin <bennylin@outlook.com>
2014-12-30 15:35:18 +0800

    V1

---
+++ src/com/company/SimpleTest.java
@@ -0,0 +1,14 @@
+package com.company;
+
+/**
+ * Created by blin on 2014/12/30.
+ */
+public class SimpleTest {
+    public static void main(String[] args){
+            System.out.println("11111");
+
+
+
+
+    }
+}
