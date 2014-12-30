9354b7e49cb82b5a4c78bfa98b19e09b45c2cf48
blin <bennylin@outlook.com>
2014-12-30 15:37:29 +0800

    V2

--- src/com/company/SimpleTest.java
+++ src/com/company/SimpleTest.java
@@ -5,7 +5,7 @@ package com.company;
  */
 public class SimpleTest {
     public static void main(String[] args){
-            System.out.println("11222111");
+            System.out.println("1122233111");
 
 
 
