cec14ebc161f95fff5cabd37496260e19d8c18df
bengal2009 <bengal168>
2014-12-31 20:59:32 +0800

    Home

--- src/com/company/SimpleTest.java
+++ src/com/company/SimpleTest.java
@@ -1,15 +1,30 @@
 package com.company;
 
+import sun.misc.Sort;
+
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.io.*;
-
+import java.util.*;
 /**
  * Created by blin on 2014/12/30.
  */
 public class SimpleTest {
+    public static void SortTest() {
+        int[] a1 = {1, 2, 3, 5, 4};
+        for (int i = 0; i < a1.length; i++) {
+            System.out.println(a1[i]);
+        }
+        System.out.println("Start Sort");
+        Arrays.sort(a1);
+        for (int i = 0; i < a1.length; i++) {
+            System.out.println(a1[i]);
+        }
+    }
+
     public static void main(String[] args) throws IOException  {
-        int[] a1=new int[5];
+       SortTest();
+      /*  int[] a1=new int[5];
         BufferedReader keyin = new BufferedReader(
                 new InputStreamReader(System.in));
         for(int i=0;i<a1.length;i++) {
@@ -24,7 +39,7 @@ public class SimpleTest {
 
             }
         }
-        System.out.println("Hello");
+        System.out.println("Hello");*/
 
 
            /* int i;
