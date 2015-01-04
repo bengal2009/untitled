bab25994124f08c9dce9a186ff41846fb18c5d81
blin <bennylin@outlook.com>
2015-01-04 14:33:12 +0800

    test

--- src/GUI2.java
+++ src/GUI2.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 /**
  * Created by blin on 2015/1/4.
@@ -12,13 +14,19 @@ public class GUI2 {
     private JPanel panel1;
     private JButton SendBut;
     private JTextField tx1;
+    private JLabel label1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+
     private enum ButtonName {
         SendBut,ClearBut;
     }
 
     public GUI2() {
 
-
+            label1.setIcon(new ImageIcon("c:\\email.png"));
         SendBut.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -39,9 +47,26 @@ public class GUI2 {
                     tx1.setText("");
             }
         });
+        panel1.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                int posx=label1.getX();
+                int posy=label1.getY();
+                super.keyPressed(e);
+                switch (e.getKeyCode()){
+                    case KeyEvent.VK_LEFT:
+                        System.out.println("Test");
+                        break;
+
+                }
+            }
+        });
+
     }
 
     public static void main(String[] args) {
+
+
         JFrame frame = new JFrame("This is a Test");
         frame.setLocation(100, 100);
         frame.setSize(400, 400);
@@ -49,6 +74,7 @@ public class GUI2 {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+
     }
 
 
