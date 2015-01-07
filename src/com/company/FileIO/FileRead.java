e2d9f1166946cfabda01e7625b31f7f8130d9f50
blin <bennylin@outlook.com>
2015-01-07 13:28:01 +0800

    Test3

---
+++ src/com/company/FileIO/FileRead.java
@@ -0,0 +1,54 @@
+package com.company.FileIO;
+import java.io.*;
+/**
+ * Created by blin on 2015/1/7.
+ */
+public class FileRead {
+    public static void main(String args[]) {
+        File f = new File("c:\\temp.txt");
+        Writer out = null;
+        try {
+            out = new FileWriter(f);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // 声明一个String类型对象
+        String str = "Hello World!!!";
+        try {
+            // 将str内容写入到文件之中
+            out.write(str);
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        try {
+            out.close();
+        } catch (IOException e2) {
+            e2.printStackTrace();
+        }
+
+        // 以下为读文件操作
+        Reader in = null;
+        try {
+            in = new FileReader(f);
+        } catch (FileNotFoundException e3) {
+            e3.printStackTrace();
+        }
+        // 开辟一个空间用于接收文件读进来的数据
+        char c1[] = new char[1024];
+        int i = 0;
+        try {
+            // 将c1的引用传递到read()方法之中，同时此方法返回读入数据的个数
+            i = in.read(c1);
+        } catch (IOException e4) {
+            e4.printStackTrace();
+        }
+        try {
+            in.close();
+        } catch (IOException e5) {
+            e5.printStackTrace();
+        }
+        //将字符数组转换为字符串输出
+        System.out.println(new String(c1, 0, i));
+    }
+
+}
---
+++ src/com/company/FileIO/RandomAccess.java
@@ -0,0 +1,67 @@
+package com.company.FileIO;
+import java.io.*;
+/**
+ * Created by blin on 2015/1/7.
+ */
+public class RandomAccess {
+    static class Employee {
+        String name;
+        int age;
+        final static int LEN = 8;
+                        public Employee(String name,int age){
+
+                            if(name.length()>LEN)
+                            {
+                            name=name.substring(0,8);
+                            }
+                            else
+                            {
+                            while(name.length()<LEN)
+                            name=name+"\u0000";
+                            }
+                            this.name=name;
+                            this.age=age;
+                        }
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        Employee e1 = new Employee("zhangsan", 23);
+        Employee e2 = new Employee("lisi", 24);
+        Employee e3 = new Employee("wangwu", 25);
+        RandomAccessFile ra = new RandomAccessFile("c:\\employee.txt", "rw");
+        ra.write(e1.name.getBytes());
+        ra.writeInt(e1.age);
+        ra.write(e2.name.getBytes());
+        ra.writeInt(e2.age);
+        ra.write(e3.name.getBytes());
+        ra.writeInt(e2.age);
+        ra.write(e3.name.getBytes());
+        ra.writeInt(e3.age);
+        ra.close();
+        RandomAccessFile raf = new RandomAccessFile("c:\\employee.txt", "r");
+      int len = 8;
+      raf.skipBytes(12); // 跳过第一个员工的信息,其姓名8字节,年龄4字节
+      System.out.println("第二个员工信息:");
+      String str = "";
+      for (int i = 0; i < len; i++)
+           str = str + (char) raf.readByte();
+        System.out.println("name:" + str);
+        System.out.println("age:" + raf.readInt());
+        System.out.println("第一个员工的信息:");
+        raf.seek(0); // 将文件指针移动到文件开始位置
+        str = "";
+        for (int i = 0; i < len; i++)
+            str = str + (char) raf.readByte();
+         System.out.println("name:" + str);
+         System.out.println("age:" + raf.readInt());
+         System.out.println("第三个员工的信息:");
+         raf.skipBytes(12); // 跳过第二个员工信息
+         str = "";
+         for (int i = 0; i < len; i++)
+            str = str + (char) raf.readByte();
+        System.out.println("name:" + str.trim());
+        System.out.println("age:" + raf.readInt());
+        raf.close();
+    }
+}
